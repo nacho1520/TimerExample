bfe4c4ced25c4121f1bdff3e74339bac108601a9
Ignacio Lopez <ignaciolopestela12@gmail.com>
2023-03-01 14:39:41 -0300

    import uuid for timers id

--- src/components/TimersDashboard/TimersDashboard.jsx
+++ src/components/TimersDashboard/TimersDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import EditableTimerList from "../EditableTimerList/EditableTimerList";
 import ToggleableTimerForm from "../ToggleableTimerForm/ToggleableTimerForm";
+import uuid from "react-uuid";
 
 const TimersDashboard = () => {
     const [ timers, setTimers ] = useState();
@@ -10,14 +11,14 @@ const TimersDashboard = () => {
             {
                 title: 'Practice squat',
                 project: 'Gym Chores',
-                id: 1,
+                id: uuid(),
                 elapsed: 5456099,
                 runningSince: Date.now()
             },
             {
                 title: 'Bake squat',
                 project: 'Kitchen Chores',
-                id: 2,
+                id: uuid(),
                 elapsed: 1273998,
                 runningSince: null
             }
