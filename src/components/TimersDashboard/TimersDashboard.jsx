6cc1227ad14a6cd502affb6d39f26587c00c6929
Ignacio Lopez <ignaciolopestela12@gmail.com>
2023-03-01 14:33:41 -0300

    Add timers data state to TimersDashboard component

--- src/components/TimersDashboard/TimersDashboard.jsx
+++ src/components/TimersDashboard/TimersDashboard.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import EditableTimerList from "../EditableTimerList/EditableTimerList";
 import ToggleableTimerForm from "../ToggleableTimerForm/ToggleableTimerForm";
 
 const TimersDashboard = () => {
+    const [ timers, setTimers ] = useState();
+
+    useEffect(() => {
+        const staticTimers = [
+            {
+                title: 'Practice squat',
+                project: 'Gym Chores',
+                id: 1,
+                elapsed: 5456099,
+                runningSince: Date.now()
+            },
+            {
+                title: 'Bake squat',
+                project: 'Kitchen Chores',
+                id: 2,
+                elapsed: 1273998,
+                runningSince: null
+            }
+        ];
+        setTimers(staticTimers);
+    }, []);
+
     return(
         <div className="ui three column centered grid">
             <div className="column">
-                <EditableTimerList />
+                <EditableTimerList timers={ timers } />
                 <ToggleableTimerForm isOpen={ true } />
             </div>
         </div>
