6c974f46d17c905f8299a24bf96872f47e509953
Ignacio Lopez <ignaciolopestela12@gmail.com>
2023-03-01 10:05:09 -0300

    EditableTimer

--- src/components/EditableTimer/EditableTimer.jsx
+++ src/components/EditableTimer/EditableTimer.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import Timer from "../Timer/Timer";
+import TimerForm from "../TimerForm/TimerForm";
 
-const EditableTimer = () => {
-    return(
-        <div>
-            EditableTimer
-        </div>
-    );
+const EditableTimer = props => {
+    if(props.editFormOpen) {
+        return(
+            <TimerForm
+                title = { props.title }
+                project = { props.project }
+            />
+        );
+    } else {
+        return(
+            <Timer 
+                title = { props.title }
+                project = { props.project }
+                elapsed = { props.elapsed }
+                runningSince = { props.runningSince }
+            />
+        );
+    }
 }
 
 export default EditableTimer;
---
+++ src/components/TimerForm/TimerForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TimerForm = () => {
+    return(
+        <div>
+            TimerForm
+        </div>
+    );
+}
+
+export default TimerForm;
