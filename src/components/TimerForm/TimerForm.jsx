ed591d4c36f2869310f2f8f750257de411e4f9dc
Ignacio Lopez <ignaciolopestela12@gmail.com>
2023-03-01 16:04:40 -0300

    Add State to ToggleableTimerForm and TimerForm component

--- src/components/TimerForm/TimerForm.jsx
+++ src/components/TimerForm/TimerForm.jsx
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const TimerForm = props => {
+    const [ state, setState ] = useState({
+        title: props.title ? props.title : '',
+        project: props.project ? props.project : ''
+    });
+
+    const handleTitleChange = e => {
+        const updateState = Object.assign({}, state, { title: e.target.value });
+        setState(updateState);
+    }
+
+    const handleProjectChange = e => {
+        const updateState = Object.assign({}, state, { project: e.target.value });
+        setState(updateState);
+    }
+
     const submitText = props.title ? 'Update' : 'Create';
     return(
         <div className="ui centered card">
-            <div>
-                <div className="content">
-                    <div className="ui form">
-                        <div className="field">
-                            <label>
-                                Title
-                            </label>
-                            <input type='text' defaultValue={ props.title } />
-                        </div>
-                        <div className="field">
-                            <label>
-                                Project
-                            </label>
-                            <input type='text' defaultValue={ props.project } />
-                        </div>
-                        <div className="ui two bottom attached buttons">
-                            <button className="ui basic blue button">
-                                { submitText }
-                            </ button>
-                            <button className="ui basic red button">
-                                Cancel
-                            </button>
-                        </div>
+            <div className="content">
+                <div className="ui form">
+                    <div className="field">
+                        <label>
+                            Title
+                        </label>
+                        <input type='text' value={ state.title } onChange={handleTitleChange} />
+                    </div>
+                    <div className="field">
+                        <label>
+                            Project
+                        </label>
+                        <input type='text' value={ state.project } onChange={handleProjectChange} />
+                    </div>
+                    <div className="ui two bottom attached buttons">
+                        <button className="ui basic blue button">
+                            { submitText }
+                        </ button>
+                        <button className="ui basic red button">
+                            Cancel
+                        </button>
                     </div>
                 </div>
             </div>
--- src/components/ToggleableTimerForm/ToggleableTimerForm.jsx
+++ src/components/ToggleableTimerForm/ToggleableTimerForm.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TimerForm from "../TimerForm/TimerForm";
 
 const ToggleableTimerForm = props => {
-    if(props.isOpen) {
+    const [ isOpen, setIsOpen ] = useState(null);
+
+    useEffect(() => {
+        setIsOpen(false);
+    }, []);
+
+    const handleFormOpen = () => {
+        setIsOpen(true);
+    }
+
+    if(isOpen) {
         return(
             <TimerForm />
         );
     } else {
         return(
             <div className="ui basic content center aligned segment">
-                <button className="ui basic button icon">
+                <button className="ui basic button icon" onClick={handleFormOpen}>
                     <i className="plus icon" />
                 </button>
             </div>
